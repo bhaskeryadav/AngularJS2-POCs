492d881c65bae97e7304ab4c93f1f10ed395cefe
Bhasker Yadav <bhaskerpyadav@gmail.com>
2016-02-06 14:15:07 +0530

    Service modifications

---
+++ AngularComponentsService/src/main/java/com/angular/components/web/dummy/pojo/Person.java
@@ -0,0 +1,15 @@
+package com.angular.components.web.dummy.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Person {
+
+	private String name;
+
+	private Integer id;
+
+	private String salary;
+}
--- AngularComponentsService/src/main/java/com/angular/components/web/service/autocomplete/AutoCompleteService.java
+++ AngularComponentsService/src/main/java/com/angular/components/web/service/autocomplete/AutoCompleteService.java
@@ -1,13 +1,69 @@
-//package com.angular.components.web.boot;
 package com.angular.components.web.service.autocomplete;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.angular.components.web.dummy.pojo.Person;
+
 
 @RestController
 public class AutoCompleteService {
 
+	Log log=LogFactory.getLog(AutoCompleteService.class.getName());
+	
+	List<Person> dummyData=new ArrayList<>();
+	
+	/**
+	 * Load dummy data
+	 */
+	public AutoCompleteService(){
+		dummyData.add(new Person("Bhasker",1,"20000.00"));
+		dummyData.add(new Person("Aaron",2,"30000.00"));
+		dummyData.add(new Person("Bundu",1,"20000.00"));
+		dummyData.add(new Person("Ankita",2,"30000.00"));
+		dummyData.add(new Person("Archana",1,"20000.00"));
+		dummyData.add(new Person("Bhautik",2,"30000.00"));
+		dummyData.add(new Person("Sudhakar",1,"20000.00"));
+		dummyData.add(new Person("Priyanka",2,"30000.00"));
+		dummyData.add(new Person("Aditya",1,"20000.00"));
+		dummyData.add(new Person("Rajesh",2,"30000.00"));
+	}
+	
+	//TODO: execute the filtering logic
+	/**
+	 * Returns the filtered data 
+	 * @param search -- string using which filteration will be done 
+	 * @return
+	 */
+	@CrossOrigin()
+	@RequestMapping(value = { "/getAutoCompleteData" },method = RequestMethod.GET,headers="Accept=application/json")
+	public  List<Person> getAutoCompleteData(@RequestParam(value="search", defaultValue="*") String search ){
+		log.info("getAutoCompleteData is invoked with parameter "+search);
+		
+		//return all data if search string is *
+		if(search.equals("*")){
+			return dummyData;
+		}
+		
+		List<Person> filteredList=dummyData.stream().filter(person-> person.getName().contains(search)).collect(Collectors.toList());
+		
+		return filteredList;
+	}
+	
+	
+	/**
+	 * Testing service
+	 * @return
+	 */
 	@CrossOrigin
 	@RequestMapping("/getData")
 	public String getData(){
