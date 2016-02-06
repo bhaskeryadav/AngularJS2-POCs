23e725b37697a53c894ed3edbc0f46a7e3d60beb
Bhasker Yadav <bhaskerpyadav@gmail.com>
2016-02-06 11:57:49 +0530

    Renaming

---
+++ AngularWebService/src/main/java/com/acn/hps/ws/service/WebServiceDApplication.java
@@ -0,0 +1,12 @@
+package com.acn.hps.ws.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceDApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiceDApplication.class, args);
+	}
+}
